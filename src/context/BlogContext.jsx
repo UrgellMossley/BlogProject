7698811300da951addec0e8e4246d9d55d563ee9
UrgellMossley <effiom123@gmail.com>
2022-03-03 23:09:18 +0100

    change request endpoints

--- src/context/BlogContext.jsx
+++ src/context/BlogContext.jsx
@@ -16,7 +16,7 @@ export const BlogProvider = ({ children })=>{
     const articleData = async(name)=>{
         try {
             //fetches article list from json server
-            const articles = await fetch("/articles");
+            const articles = await fetch("https://json-server-test-master.herokuapp.com/articles");
             const result = await articles.json();
             //awaits and parse the JSON, then use filter to use passed in article name to pick out desired article
             const articleObj = await result.filter((item) => item.title === name)
@@ -32,7 +32,7 @@ export const BlogProvider = ({ children })=>{
     //simple async function that pulls out article list from JSON server
     const artcleArr = async () => {
         try {
-            const articles = await fetch("/articles")
+            const articles = await fetch("https://json-server-test-master.herokuapp.com/articles")
             const res = await articles.json()
             return dispatch({ type: "SET_ARTICLES", payload: res })
         } catch (error) {
@@ -43,7 +43,7 @@ export const BlogProvider = ({ children })=>{
     //async function that queries the articles in DB and sorts by date tag, returns the link information of the first index (latest article)
     const latestArr= async ()=>{
         try {
-            const articles = await fetch("/articles");
+            const articles = await fetch("https://json-server-test-master.herokuapp.com/articles");
             const result = await articles.json();
             const dateSortedArr = result.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
             const latest = dateSortedArr[0]
