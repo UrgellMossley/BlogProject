6a4168d4b7d3b0033db181512602afa73c4640ef
UrgellMossley <effiom123@gmail.com>
2022-03-03 08:42:53 +0100

    error handling on promises

--- src/context/BlogContext.jsx
+++ src/context/BlogContext.jsx
@@ -14,31 +14,44 @@ export const BlogProvider = ({ children })=>{
     }
     //Fucntion to pullout individual article
     const articleData = async(name)=>{
-        //fetches article list from json server
-        const articles = await fetch("/articles");
-        const result = await articles.json();
-        //awaits and parse the JSON, then use filter to use passed in article name to pick out desired article
-        const articleObj = await result.filter((item) => item.title === name)
-        //Normalise data
-        const res = articleObj[0]
-        //set context
-        return dispatch({type: "GET_ARTICLE", payload: res})
+        try {
+            //fetches article list from json server
+            const articles = await fetch("/articles");
+            const result = await articles.json();
+            //awaits and parse the JSON, then use filter to use passed in article name to pick out desired article
+            const articleObj = await result.filter((item) => item.title === name)
+            //Normalise data
+            const res = articleObj[0]
+            //set context
+            return dispatch({ type: "GET_ARTICLE", payload: res })
+        } catch (error) {
+            console.log(error)
+        }
+     
     }
     //simple async function that pulls out article list from JSON server
     const artcleArr = async () => {
-        const articles = await fetch("/articles")
-        const res = await articles.json()
-        return dispatch({ type: "SET_ARTICLES", payload: res})
+        try {
+            const articles = await fetch("/articles")
+            const res = await articles.json()
+            return dispatch({ type: "SET_ARTICLES", payload: res })
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
     //async function that queries the articles in DB and sorts by date tag, returns the link information of the first index (latest article)
     const latestArr= async ()=>{
-        const articles = await fetch("/articles");
-        const result = await articles.json();
-        const dateSortedArr = result.sort((a, b) => {
-            return new Date(b.date).getTime() - new Date(a.date).getTime()
-        })
-        const latest = dateSortedArr[0]
-        return dispatch({type: `GET_LATEST_ARTICLE`, payload: latest.link})
+        try {
+            const articles = await fetch("/articles");
+            const result = await articles.json();
+            const dateSortedArr = result.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+            const latest = dateSortedArr[0]
+            return dispatch({ type: `GET_LATEST_ARTICLE`, payload: latest.link })
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
    
     
